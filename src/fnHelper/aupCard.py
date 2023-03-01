c3810387990d7c66ed0e7c60a5c7815dc29b2b14
Nico <gabo.janicolo@gmail.com>
2023-03-02 00:56:16 +0800

    disabled temporarily

--- src/fnHelper/aupCard.py
+++ src/fnHelper/aupCard.py
@@ -1,16 +1,16 @@
-from smartcard.CardType import CardType
-from smartcard.CardRequest import CardRequest
-from smartcard.util import toHexString
+# from smartcard.CardType import CardType
+# from smartcard.CardRequest import CardRequest
+# from smartcard.util import toHexString
 
-class DCCardType(CardType):
-    def matches( self, atr, reader=None ):
-        return atr[0]==0x3B
+# class DCCardType(CardType):
+#     def matches( self, atr, reader=None ):
+#         return atr[0]==0x3B
 
-cardtype = DCCardType()
-cardrequest = CardRequest( timeout=1, cardType=cardtype )
-cardservice = cardrequest.waitforcard()
+# cardtype = DCCardType()
+# cardrequest = CardRequest( timeout=1, cardType=cardtype )
+# cardservice = cardrequest.waitforcard()
 
-cardservice.connection.connect()
-print (toHexString( cardservice.connection.getATR() ))
+# cardservice.connection.connect()
+# print (toHexString( cardservice.connection.getATR() ))
 
-print (cardservice.connection.getReader())
+# print (cardservice.connection.getReader())
--- src/fnHelper/otp.py
+++ src/fnHelper/otp.py
@@ -2,4 +2,3 @@ import pyotp
 
 class OTPAuthenticate():
     totp = pyotp.TOTP('elcidelcidelcid')
-    print(totp.now())
