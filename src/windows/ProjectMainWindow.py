56873d4f4ec2eb2bd73e38bd7dc44884c4b6987d
kejacanedo <77665335+kejacanedo@users.noreply.github.com>
2023-03-09 23:22:08 +0800

    Navigation from login to user type menu.

--- src/windows/ProjectMainWindow.py
+++ src/windows/ProjectMainWindow.py
@@ -16,17 +16,30 @@ class ProjectMainWindow(QMainWindow, Ui_ProjectMainWindow):
         self.actionLogout.triggered.connect(lambda: self.logout())
 
     def loginAttempt(self):
-        print("hello world")
+        # print("hello world")
         user = login.login_attempt(self.lineSchoolId_login.text(), self.linePassword_login.text())
-        self.stackedWidget.setCurrentIndex(4)
+        try:
+            match user['user_type']:
+                case "user":
+                    self.stackedWidget.setCurrentIndex(1)
+                case "business":
+                    self.stackedWidget.setCurrentIndex(2)
+                case "teller":
+                    self.stackedWidget.setCurrentIndex(3)
+                case "admin":
+                    self.stackedWidget.setCurrentIndex(4)
+        except TypeError as e:
+            print("")
+
+
 
     def logout(self):
         logout.Logout(self)
 
-# class AdministratorWindow(ProjectMainWindow):
-#     def __init__(self, parent=ProjectMainWindow):
-#         super(ProjectMainWindow).__init__(parent)
-#         self.buttonAddUser_administrator.clicked.connect(lambda: self.bro())
+class AdministratorWindow(ProjectMainWindow):
+    def __init__(self, parent=ProjectMainWindow):
+        super(ProjectMainWindow).__init__(parent)
+        self.buttonAddUser_administrator.clicked.connect(lambda: self.bro())
         
-#     def bro(self):
-#         print("bro")
+    def bro(self):
+        print("bro")
