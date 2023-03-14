8f72ab5c0afe8ef87c0273f3b065372899ae27e3
Nico <gabo.janicolo@gmail.com>
2023-03-14 21:43:54 +0800

    disabled aupcard temporarily

--- src/fnHelper/__init__.py
+++ src/fnHelper/__init__.py
@@ -1,6 +1,10 @@
-__all__ = ['login', 'logout', 'aupCard', 'otpAuth']
+__all__ = ['login', 
+           'logout', 
+        #    'aupCard', 
+           'otpAuth',
+           ]
 
-from .aupCard import *
+# from .aupCard import *
 from .login import *
 from .logout import *
 from .otpAuth import *
