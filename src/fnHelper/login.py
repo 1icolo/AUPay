d382b798bb0ce7d3e3966e627c12031b6909a66e
Nico <gabo.janicolo@gmail.com>
2023-03-16 23:34:05 +0800

    rfid login function working

--- src/fnHelper/login.py
+++ src/fnHelper/login.py
@@ -1,5 +1,10 @@
 from dbHelper import find_user_by_login
+from dbHelper import find_user_by_card_id
 
 def login_attempt(id, password):
     user = find_user_by_login(id, password)
+    return user
+
+def login_rfid(card_id):
+    user = find_user_by_card_id(card_id)
     return user
