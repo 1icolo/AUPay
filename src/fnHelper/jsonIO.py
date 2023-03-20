389af43262338e2ddbab55824d6c523547c3e5aa
Nico <gabo.janicolo@gmail.com>
2023-03-20 22:29:37 +0800

    added jsonIO for items.json for business

---
+++ src/fnHelper/jsonIO.py
@@ -0,0 +1,13 @@
+import json
+
+
+def read_items(file: str = 'items.json', mode: str = 'r' ):
+    with open(file, 'r') as f:
+        return json.load(f)
+    
+
+def write_items(newData: dict, file: str = 'items.json', mode: str = 'w' ):
+    with open(file, 'w') as f:
+        json.dump(newData, f)
+        print(f'JSON file {file} updated')
+
