0cdd4824e7277e684aca7f869487eb338043b59a
Nico <gabo.janicolo@gmail.com>
2023-03-21 22:47:04 +0800

    added a new query: find all transactions of a specific user

--- src/dbHelper/find_transaction.py
+++ src/dbHelper/find_transaction.py
@@ -11,8 +11,8 @@ def find_transaction(transaction_id):
     except:
         print("Transaction doesn't exist")
         return None
-    
-    
+
+
 def find_all_transactions():
     try:
         transactions = Database().collection['transactions'].find()
@@ -21,16 +21,33 @@ def find_all_transactions():
         print("No transaction exists in the database.")
         return None
 
+
+def find_all_transactions_of_user(_id):
+    _id = ObjectId(_id)
+    try:
+        transactions = Database().collection['transactions'].find({
+            "$or": [
+                {'source_id': _id},
+                {'destination_id': _id}
+            ]
+        })
+        return transactions
+    except:
+        print("Finding transactions failed.")
+        return None
+
+
 def load_transaction_table(self):
-        load_transactions = Database().collection['transactions'].find()
-        transaction_data = []
-        transactions_data = []
-        for transactions in load_transactions:
-            transaction_data.append([transactions['timestamp']])       
-            bson_timestamp = transaction_data[0][0]
-            from datetime import datetime
-            dt = datetime.fromtimestamp(bson_timestamp.time)
-            date_string = dt.strftime("%m/%d/%y")
-            transactions_data.append([transactions['_id'],date_string,transactions['source_id'],transactions['destination_id'],transactions['amount'],transactions['description']])
-        # print(transactions_data)
-        return transactions_data
+    load_transactions = Database().collection['transactions'].find()
+    transaction_data = []
+    transactions_data = []
+    for transactions in load_transactions:
+        transaction_data.append([transactions['timestamp']])
+        bson_timestamp = transaction_data[0][0]
+        from datetime import datetime
+        dt = datetime.fromtimestamp(bson_timestamp.time)
+        date_string = dt.strftime("%m/%d/%y")
+        transactions_data.append([transactions['_id'], date_string, transactions['source_id'],
+                                 transactions['destination_id'], transactions['amount'], transactions['description']])
+    # print(transactions_data)
+    return transactions_data
