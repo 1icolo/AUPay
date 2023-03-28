a55d1cebccc559cd55636072752f653f9ce48e90
Nico <gabo.janicolo@gmail.com>
2023-03-28 22:54:54 +0800

    fixed login

--- src/windows/ProjectMainWindow.py
+++ src/windows/ProjectMainWindow.py
@@ -11,42 +11,24 @@ class ProjectMainWindow(QMainWindow, Ui_ProjectMainWindow):
     def __init__(self, parent=None):
         super(ProjectMainWindow, self).__init__(parent)
         self.setupUi(self)
-        self.loginRFID(1)
+        self.loginAttempt('rfid')
         self.actionLogout.triggered.connect(lambda: self.logoutAttempt())
-        self.buttonLogin_login.clicked.connect(lambda: self.loginAttempt())
-        self.buttonRFIDLogin_login.clicked.connect(lambda: self.loginRFID())
+        self.buttonLogin_login.clicked.connect(lambda: self.loginAttempt('password'))
+        self.buttonRFIDLogin_login.clicked.connect(lambda: self.loginAttempt('rfid'))
         
     def logoutAttempt(self):
         QApplication.quit()
         python = sys.executable
         os.execl(python, python, *sys.argv)
 
-    def loginAttempt(self):
-        user = login.login_attempt(self.lineSchoolId_login.text(), hashEncryption.encrypt(self.linePassword_login.text()))
-        # if user doesn't match anything
-        if user is None:
-            return
-        match user['user_type']:
-            case 'admin':
-                self.stackedWidget.setCurrentIndex(4)
-                from windows.AdminWindow import AdminWindow
-                AdminWindow(self)
-            case 'user':
-                self.stackedWidget.setCurrentIndex(1)
-                from windows.UserWindow import UserWindow
-                UserWindow(self, user)
-            case 'business':
-                self.stackedWidget.setCurrentIndex(2)
-                from windows.BusinessWindow import BusinessWindow
-                BusinessWindow(self, user['_id'])
-            case 'teller':
-                self.stackedWidget.setCurrentIndex(3)
-                from windows.TellerWindow import TellerWindow
-                TellerWindow(self, user['_id'])
+    def loginAttempt(self, mode):
+        try: 
+            user = None
+            match mode:
+                case 'password': user = login.login_attempt(self.lineSchoolId_login.text(), hashEncryption.encrypt(self.linePassword_login.text()))
+                case 'rfid': user = login.login_rfid(hashEncryption.encrypt(AUPCard(0.001).get_uid())) 
 
-    def loginRFID(self, seconds = 5):
-        try:
-            user = login.login_rfid(hashEncryption.encrypt(AUPCard(seconds).get_uid()))  
+            # if user doesn't match anything
             if user is None:
                 return
             match user['user_type']:
@@ -61,10 +43,10 @@ class ProjectMainWindow(QMainWindow, Ui_ProjectMainWindow):
                 case 'business':
                     self.stackedWidget.setCurrentIndex(2)
                     from windows.BusinessWindow import BusinessWindow
-                    BusinessWindow(self)
+                    BusinessWindow(self, user['_id'])
                 case 'teller':
                     self.stackedWidget.setCurrentIndex(3)
                     from windows.TellerWindow import TellerWindow
-                    TellerWindow(self)
-        except:
-            pass
+                    TellerWindow(self, user['_id'])
+        except Exception:
+            print('No RFID detected.')
