e0f938d6501cda66df748ea2b997d9aadf61df7d
Nico <gabo.janicolo@gmail.com>
2023-03-29 00:23:04 +0800

    changed return data type from cursor to double

--- src/dbHelper/compute_user_balance.py
+++ src/dbHelper/compute_user_balance.py
@@ -3,27 +3,26 @@ from bson import ObjectId
 
 
 def compute_user_balance(user_id):
-    user_id = ObjectId(user_id)
     balance = Database().collection['transactions'].aggregate([
         {
             '$match': {
                 '$or': [
                     {
-                        'source_id': user_id
+                        'source_id': ObjectId(user_id)
                     }, {
-                        'destination_id': user_id
+                        'destination_id': ObjectId(user_id)
                     }
                 ]
             }
         }, {
             '$group': {
-                '_id': user_id,
+                '_id': ObjectId(user_id),
                 'balance': {
                     '$sum': {
                         '$cond': [
                             {
                                 '$eq': [
-                                    '$destination_id', user_id
+                                    '$destination_id', ObjectId(user_id)
                                 ]
                             }, '$amount', {
                                 '$multiply': [
@@ -36,4 +35,5 @@ def compute_user_balance(user_id):
             }
         }
     ])
-    return balance
+    for result in balance:
+        return result['balance']
