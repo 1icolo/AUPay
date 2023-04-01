d49f01ae572a4a4302093237f7f020ae3fd18897
kejacanedo <77665335+kejacanedo@users.noreply.github.com>
2023-04-01 23:59:05 +0800

    load_user_transaction_by_id

--- src/fnHelper/load_tables.py
+++ src/fnHelper/load_tables.py
@@ -2,7 +2,7 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from dbHelper.find_user import find_all_users
-from dbHelper.find_transaction import find_all_transactions
+from dbHelper.find_transaction import *
 # import json
 from fnHelper import jsonIO
 
@@ -68,4 +68,26 @@ def load_inventory_to_table(self, tableWidget):
     for row in range(rows):
         for column in range(columns):
             item = QTableWidgetItem(str(items_data[row][column]))
+            tableWidget.setItem(row, column, item)
+
+def load_user_transaction_by_id(self, tableWidget, user):
+    transactions = find_all_transactions_of_user(user)
+    transaction_data = []
+    transactions_data = []
+    for transaction in transactions:
+        transaction_data.append([transaction['timestamp']])
+        bson_timestamp = transaction_data[0][0]
+        from datetime import datetime
+        dt = datetime.fromtimestamp(bson_timestamp.time)
+        date_string = dt.strftime("%m/%d/%y")
+        transactions_data.append([transaction['_id'], date_string, transaction['source_id'],
+                                 transaction['destination_id'], transaction['amount'], transaction['description']])
+    # print(transactions_data)
+    rows = len(transactions_data)
+    columns = len(transactions_data[0])
+    tableWidget.setRowCount(len(transactions_data))
+    # Add the user data to the table
+    for row in range(rows):
+        for column in range(columns):
+            item = QTableWidgetItem(str(transactions_data[row][column]))
             tableWidget.setItem(row, column, item)
