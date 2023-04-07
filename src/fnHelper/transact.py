cbbd2d799f61f7a9ab25ccedf74fae42d607a317
Nico <gabo.janicolo@gmail.com>
2023-04-07 16:21:55 +0800

    added balance RFID and sufficiency checking for teller

--- src/fnHelper/transact.py
+++ src/fnHelper/transact.py
@@ -6,33 +6,38 @@ from fnHelper.aupCard import AUPCard
 from fnHelper.hashEncryption import encrypt
 from dbHelper.find_user import find_user_by_card_id
 from fnHelper.output_to_dict import output_to_dict
+from fnHelper.checkBalanceSufficiency import checkBalanceSufficiency
+from PyQt5.QtWidgets import QMessageBox
+
 
 def transact(Widget, teller):
     user = find_user_by_card_id(encrypt(AUPCard().get_uid()))
     if user is None:
         return print("No RFID detected.")
-    if(Widget.comboTransaction_teller.currentText() == "Deposit"):
-        print("Deposit")
-        newTransaction = {
-            "timestamp": Timestamp(int(datetime.today().timestamp()), 1),
-            "source_id": ObjectId(teller['_id']),
-            "destination_id": ObjectId(user['_id']),
-            "amount": float(Widget.tellerWindow_amountLine.text()),
-            "description": Widget.tellerWindow_descriptionLine.toPlainText()
-        }
-    elif(Widget.comboTransaction_teller.currentText() == "Withdraw"):
-        print("Withdraw")
-        newTransaction = {
-            "timestamp": Timestamp(int(datetime.today().timestamp()), 1),
-            "source_id": ObjectId(user['_id']),
-            "destination_id": ObjectId(teller['_id']),
-            "amount": float(Widget.tellerWindow_amountLine.text()),
-            "description": Widget.tellerWindow_descriptionLine.toPlainText()
-        }
-    add_transaction(newTransaction)
-        
-    load_user_transaction_by_id(Widget.tellerWindow_transactions_table, teller['_id'])
-    Widget.tellerWindow_transactions_table.setCurrentItem(None)
-    Widget.tellerWindow_schoolIdLine.setText("")
-    Widget.tellerWindow_amountLine.setText("")
-    Widget.tellerWindow_descriptionLine.setText("")
+
+    newTransaction = {
+        "timestamp": Timestamp(int(datetime.today().timestamp()), 1),
+        "source_id": None,
+        "destination_id": None,
+        "amount": float(Widget.tellerWindow_amountLine.text()),
+        "description": Widget.tellerWindow_descriptionLine.toPlainText()
+    }
+
+    match Widget.comboTransaction_teller.currentText():
+        case "Deposit":
+            newTransaction['source_id'] = ObjectId(teller['_id'])
+            newTransaction['destination_id'] = ObjectId(user['_id'])
+        case "Withdraw":
+            newTransaction['source_id'] = ObjectId(user['_id'])
+            newTransaction['destination_id'] = ObjectId(teller['_id'])
+
+    if checkBalanceSufficiency(user['_id'], newTransaction['amount']):
+        add_transaction(newTransaction)
+        load_user_transaction_by_id(
+            Widget.tellerWindow_transactions_table, teller['_id']
+        )
+        Widget.tellerWindow_transactions_table.setCurrentItem(None)
+        Widget.buttonClearFields_teller.click()
+        return QMessageBox.information(Widget, "Success", "Charge successful.")
+    else:
+        return QMessageBox.critical(Widget, "Error", "Insufficient Balance.")
--- src/windows/TellerWindow.py
+++ src/windows/TellerWindow.py
@@ -30,10 +30,12 @@ def TellerWindow(self, user):
     load_user_transaction_by_id(self.tellerWindow_transactions_table, user['_id'])
     self.tellerWindow_transactions_table.itemSelectionChanged.connect(lambda: selected_row_to_textbox(self))
     self.tellerWindow_transaction_search.textChanged.connect(lambda text: search_transactions(self, text, self.tellerWindow_transactions_table))
-    self.buttonTransact_teller.clicked.connect(lambda: transact(self, user))
+    self.buttonTransact_teller.clicked.connect(lambda: transactAttempt(self, user))
     self.lineBalance_teller.setText(compute_user_balance(user['_id']))
 
         
-
+def transactAttempt(self, user):
+    if not self.tellerWindow_amountLine.text() == "" and not self.tellerWindow_descriptionLine.toPlainText() == "":
+        transact(self,user)
 
 
