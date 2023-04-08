2c8a5b62343e33e1ad86769f9af041b29f35f3e9
Nico <gabo.janicolo@gmail.com>
2023-04-09 00:27:34 +0800

    simplified the algo

--- src/dbHelper/calculate_total_circulating_supply.py
+++ src/dbHelper/calculate_total_circulating_supply.py
@@ -1,53 +1,12 @@
 from .database import Database
 from bson import ObjectId
+from .compute_user_balance import compute_user_balance
 
 
 def calculate_total_circulating_supply():
     total_circulation_supply = 0
-
     for user in Database().collection['users'].find():
         user_id = user['_id']
-        result = Database().collection['transactions'].aggregate([
-            {
-                '$match': {
-                    '$or': [
-                        {
-                            'source_id': ObjectId(user_id)
-                        }, {
-                            'destination_id': ObjectId(user_id)
-                        }
-                    ]
-                }
-            }, {
-                '$group': {
-                    '_id': ObjectId(user_id),
-                    'balance': {
-                        '$sum': {
-                            '$cond': [
-                                {
-                                    '$eq': [
-                                        '$source_id', '$destination_id'
-                                    ]
-                                }, float(0.00), {
-                                    '$cond': [
-                                        {
-                                            '$eq': [
-                                                '$destination_id', ObjectId(
-                                                    user_id)
-                                            ]
-                                        }, '$amount', {
-                                            '$multiply': [
-                                                -1, '$amount'
-                                            ]
-                                        }
-                                    ]
-                                }
-                            ]
-                        }
-                    }
-                }
-            }
-        ])
-        for doc in result:
-            total_circulation_supply += doc['balance']*-1
-            return total_circulation_supply
+        result = compute_user_balance(user_id)
+        total_circulation_supply += result
+    return total_circulation_supply
