1f6e65f0223d18191eb9c73913bc8e0fa11ac559
Nico <gabo.janicolo@gmail.com>
2023-04-09 00:26:51 +0800

    login as user checkbox

--- src/fnHelper/login.py
+++ src/fnHelper/login.py
@@ -1,10 +1,39 @@
 from dbHelper import find_user_by_login
 from dbHelper import find_user_by_card_id
+from fnHelper.hashEncryption import encrypt
+from fnHelper.aupCard import AUPCard
 
-def login_attempt(id, password):
-    user = find_user_by_login(id, password)
-    return user
+def login(self, mode):
+        try:
+            user = None
+            if mode == 'password':
+                user = find_user_by_login(self.lineSchoolId_login.text(), encrypt(self.linePassword_login.text()))
+            elif mode == 'rfid':
+                user = find_user_by_card_id(encrypt(AUPCard(0.001).get_uid()))
 
-def login_rfid(card_id):
-    user = find_user_by_card_id(card_id)
-    return user
+            if user is None:
+                return
+            
+            if self.checkBoxLoginAsClient_login.isChecked():
+                self.stackedWidget.setCurrentIndex(1)
+                from windows.UserWindow import UserWindow
+                UserWindow(self, user)
+            else:
+                if user['user_type'] == 'admin':
+                    self.stackedWidget.setCurrentIndex(4)
+                    from windows.AdminWindow import AdminWindow
+                    AdminWindow(self, user)
+                elif user['user_type'] == 'user':
+                    self.stackedWidget.setCurrentIndex(1)
+                    from windows.UserWindow import UserWindow
+                    UserWindow(self, user)
+                elif user['user_type'] == 'business':
+                    self.stackedWidget.setCurrentIndex(2)
+                    from windows.BusinessWindow import BusinessWindow
+                    BusinessWindow(self, user)
+                elif user['user_type'] == 'teller':
+                    self.stackedWidget.setCurrentIndex(3)
+                    from windows.TellerWindow import TellerWindow
+                    TellerWindow(self, user)
+        except Exception as e:
+            print(f"Login Failed. \n{e}")
--- src/windows/ProjectMainWindow.py
+++ src/windows/ProjectMainWindow.py
@@ -6,6 +6,7 @@ from windows.ui.ui_ProjectMainWindow import Ui_ProjectMainWindow
 from fnHelper.aupCard import AUPCard
 import sys, os
 from fnHelper import hashEncryption
+from fnHelper.login import login
 
 class ProjectMainWindow(QMainWindow, Ui_ProjectMainWindow):
     def __init__(self, parent=None):
@@ -22,51 +23,4 @@ class ProjectMainWindow(QMainWindow, Ui_ProjectMainWindow):
         os.execl(python, python, *sys.argv)
 
     def loginAttempt(self, mode):
-        try: 
-            user = None
-            match mode:
-                case 'password': user = login.login_attempt(self.lineSchoolId_login.text(), hashEncryption.encrypt(self.linePassword_login.text()))
-                case 'rfid': user = login.login_rfid(hashEncryption.encrypt(AUPCard(0.001).get_uid())) 
-
-            # if user doesn't match anything
-            if user is None:
-                return
-            match user['user_type']:
-                case 'admin':
-                    if self.checkBoxLoginAsClient_login.isChecked():
-                        self.stackedWidget.setCurrentIndex(1)
-                        from windows.UserWindow import UserWindow
-                        UserWindow(self, user)
-                    else:
-                        self.stackedWidget.setCurrentIndex(4)
-                        from windows.AdminWindow import AdminWindow
-                        AdminWindow(self, user)
-                case 'user':
-                    if self.checkBoxLoginAsClient_login.isChecked():
-                        self.stackedWidget.setCurrentIndex(1)
-                        from windows.UserWindow import UserWindow
-                        UserWindow(self, user)
-                    else:
-                        self.stackedWidget.setCurrentIndex(1)
-                        from windows.UserWindow import UserWindow
-                        UserWindow(self, user)
-                case 'business':
-                    if self.checkBoxLoginAsClient_login.isChecked():
-                        self.stackedWidget.setCurrentIndex(1)
-                        from windows.UserWindow import UserWindow
-                        UserWindow(self, user)
-                    else:
-                        self.stackedWidget.setCurrentIndex(2)
-                        from windows.BusinessWindow import BusinessWindow
-                        BusinessWindow(self, user)
-                case 'teller':
-                    if self.checkBoxLoginAsClient_login.isChecked():
-                        self.stackedWidget.setCurrentIndex(1)
-                        from windows.UserWindow import UserWindow
-                        UserWindow(self, user)
-                    else:
-                        self.stackedWidget.setCurrentIndex(3)
-                        from windows.TellerWindow import TellerWindow
-                        TellerWindow(self, user)
-        except Exception:
-            print('No RFID detected.')
+        login(self, mode)
