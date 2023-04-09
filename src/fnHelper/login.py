5d250782c6779de008de284dbb9906dced9f3eb0
Nico <gabo.janicolo@gmail.com>
2023-04-10 00:49:11 +0800

    Update login.py

--- src/fnHelper/login.py
+++ src/fnHelper/login.py
@@ -4,36 +4,36 @@ from fnHelper.hashEncryption import encrypt
 from fnHelper.aupCard import AUPCard
 
 def login(self, mode):
-        try:
-            user = None
-            if mode == 'password':
-                user = find_user_by_login(self.lineSchoolId_login.text(), encrypt(self.linePassword_login.text()))
-            elif mode == 'rfid':
-                user = find_user_by_card_id(encrypt(AUPCard(0.001).get_uid()))
+    try:
+        user = None
+        if mode == 'password':
+            user = find_user_by_login(self.lineSchoolId_login.text(), encrypt(self.linePassword_login.text()))
+        elif mode == 'rfid':
+            user = find_user_by_card_id(encrypt(AUPCard(0.001).get_uid()))
 
-            if user is None:
-                return
-            
-            if self.checkBoxLoginAsClient_login.isChecked():
+        if user is None:
+            return
+
+        if self.checkBoxLoginAsClient_login.isChecked():
+            self.stackedWidget.setCurrentIndex(1)
+            from windows.UserWindow import UserWindow
+            UserWindow(self, user)
+        else:
+            if user['user_type'] == 'admin':
+                self.stackedWidget.setCurrentIndex(4)
+                from windows.AdminWindow import AdminWindow
+                AdminWindow(self, user)
+            elif user['user_type'] == 'user':
                 self.stackedWidget.setCurrentIndex(1)
                 from windows.UserWindow import UserWindow
                 UserWindow(self, user)
-            else:
-                if user['user_type'] == 'admin':
-                    self.stackedWidget.setCurrentIndex(4)
-                    from windows.AdminWindow import AdminWindow
-                    AdminWindow(self, user)
-                elif user['user_type'] == 'user':
-                    self.stackedWidget.setCurrentIndex(1)
-                    from windows.UserWindow import UserWindow
-                    UserWindow(self, user)
-                elif user['user_type'] == 'business':
-                    self.stackedWidget.setCurrentIndex(2)
-                    from windows.BusinessWindow import BusinessWindow
-                    BusinessWindow(self, user)
-                elif user['user_type'] == 'teller':
-                    self.stackedWidget.setCurrentIndex(3)
-                    from windows.TellerWindow import TellerWindow
-                    TellerWindow(self, user)
-        except Exception as e:
-            print(f"Login Failed. \n{e}")
+            elif user['user_type'] == 'business':
+                self.stackedWidget.setCurrentIndex(2)
+                from windows.BusinessWindow import BusinessWindow
+                BusinessWindow(self, user)
+            elif user['user_type'] == 'teller':
+                self.stackedWidget.setCurrentIndex(3)
+                from windows.TellerWindow import TellerWindow
+                TellerWindow(self, user)
+    except Exception as e:
+        print(f"Login Failed. \n{e}")
