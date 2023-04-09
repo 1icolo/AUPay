7001ef51f585946432f2d27598fc5651248fad4b
Nico <gabo.janicolo@gmail.com>
2023-04-10 00:56:25 +0800

    Update RFIDDialog.py

--- src/windows/RFIDDialog.py
+++ src/windows/RFIDDialog.py
@@ -3,43 +3,17 @@ from windows.ui.ui_RFIDDialog import Ui_RFIDDialog
 from smartcard.CardType import AnyCardType
 from smartcard.CardRequest import CardRequest
 from smartcard.util import toHexString
-import time
+from PyQt5.QtCore import QTimer, Qt
 
 
 class RFIDDialog(QDialog):
-
-    def __init__(self, timeout:int = 5, parent=None):
+    def __init__(self, timeout:int = 1, parent=None):
         print(__name__)
         super(RFIDDialog, self).__init__(parent)
+        self.rfidDialog()
+        QTimer.singleShot(timeout*1000, self.close)
+
+    def rfidDialog(self):
         self.ui = Ui_RFIDDialog()
         self.ui.setupUi(self)
-        time.sleep(2)
-        self.close()
-        
-    #     try:
-    #         # define the card type
-    #         cardtype = AnyCardType()
-
-    #         # request a card connection
-    #         cardrequest = CardRequest(timeout=timeout, cardType=cardtype)
-    #         cardservice = cardrequest.waitforcard()
-
-    #         # connect to the card
-    #         cardservice.connection.connect()
-
-    #         # get the UID of the card
-    #         GET_UID = [0xFF, 0xCA, 0x00, 0x00, 0x00]
-    #         self.data, sw1, sw2 = cardservice.connection.transmit(GET_UID)
-
-    #         self.close()
-            
-    #     except Exception:
-    #         self.close()
-
-    # def get_uid(self):
-    #     # return the UID in hexadecimal format
-    #     self.exec()
-    #     uid = toHexString(self.data)
-    #     # print(uid)
-    #     self.close()
-    #     return uid
+        self.setWindowFlags(Qt.WindowType.FramelessWindowHint)
