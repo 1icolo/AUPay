19a35e2ba11660e4798d16d9f781cfc326f067fd
Nico <gabo.janicolo@gmail.com>
2023-04-11 01:15:32 +0800

    Update calculate_total_circulating_supply.py

--- src/dbHelper/calculate_total_circulating_supply.py
+++ src/dbHelper/calculate_total_circulating_supply.py
@@ -4,9 +4,11 @@ from .compute_user_balance import compute_user_balance
 
 
 def calculate_total_circulating_supply():
-    total_circulation_supply = 0
+    total_circulation_supply = 0.0
     for user in Database().collection['users'].find():
         user_id = user['_id']
         result = compute_user_balance(user_id)
+        if result is None:
+            result = float(0.0)
         total_circulation_supply += result
     return total_circulation_supply
