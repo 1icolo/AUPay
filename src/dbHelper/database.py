83b090b66592e2507f87ed6c0588856f9846a70a
Nico <gabo.janicolo@gmail.com>
2023-04-11 14:52:24 +0800

    replaced fixed admin initial user id with random id

--- src/dbHelper/database.py
+++ src/dbHelper/database.py
@@ -18,7 +18,7 @@ class Database:
             'users': self.database['users'],
             'transactions': self.database['transactions']
         }
-        
+
         try:
             if not self.client.list_database_names().__contains__('aupaydb'):
                 self.__create_database()
@@ -28,32 +28,30 @@ class Database:
     # Initial collection documents
     def __create_database(self):
         coinbase = ObjectId('ffffffffffffffffffffffff')
-        admin = ObjectId('000000000000000000000000')
         self.database.create_collection('users', validator=user_schema)
-        self.database.create_collection('transactions', validator=transaction_schema)
-        
-        documents = {
-            'user': {
-                '_id': admin,
-                'card_id': "7f453b1936a11e152d5cd96c66cdd4caf13024c390509f71daf5410c1d742986",
-                'school_id': "AUP52023BSIT",
-                'password': "8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918",
-                'otp_key': "d1d3b9e6f7a4a8c8d3f5e2c3b2a1d0c7f6e5d4c3b2a190807060504030201000",
-                'user_type': 'admin',
-                'balance': float(0.00),
-            },
-            'transaction': {
-                'timestamp': Timestamp(1651363200, 0),
-                'source_id': coinbase,
-                'destination_id': admin,
-                'amount': float(0.00),
-                'description': "Coinbase transaction",
-            }
-        }
-        self.collection['users'].insert_one(documents['user'])
-        self.collection['transactions'].insert_one(documents['transaction'])
-        print("Initial database created.")
+        self.database.create_collection(
+            'transactions', validator=transaction_schema)
+        initial_user = {
+            'card_id': "7f453b1936a11e152d5cd96c66cdd4caf13024c390509f71daf5410c1d742986",
+            'school_id': "AUP52023BSIT",
+            'password': "8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918",
+            'otp_key': "d1d3b9e6f7a4a8c8d3f5e2c3b2a1d0c7f6e5d4c3b2a190807060504030201000",
+            'user_type': 'admin',
+            'balance': float(0.00),
 
+        }
+        user = self.collection['users'].insert_one(initial_user)
+
+        initial_transaction = {
+            'timestamp': Timestamp(1651363200, 0),
+            'source_id': coinbase,
+            'destination_id': user.inserted_id,
+            'amount': float(0.00),
+            'description': "Coinbase transaction",
+        }
+        transaction = self.collection['transactions'].insert_one(initial_transaction)
+
+        print("Initial database created.")
 
 user_schema = {
     '$jsonSchema': {
