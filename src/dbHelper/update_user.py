3d4500509900d787d6a73b6baa45f31f4bc1222a
elciddelapaz <elcid.delapaz@gmail.com>
2023-04-11 19:09:38 +0800

    created update_balance and update_all_balance

--- src/dbHelper/update_user.py
+++ src/dbHelper/update_user.py
@@ -1,4 +1,5 @@
 from .database import Database
+from .compute_user_balance import compute_user_balance
 
 
 def update_user(user):
@@ -9,4 +10,22 @@ def update_user(user):
         }
     )
     print(f"User {user['_id']} updated.")
+    return True
+def update_balance(user):
+    Database().collection['users'].update_one(
+        {"_id": user['_id']},
+        {"$set": {
+            "balance": compute_user_balance(user['_id'])}
+        }
+    )
+    print(f"User {user['_id']} updated.")
+    return True
+def update_all_balance(user):
+    updated = Database().collection['users'].update_many(
+        {"_id": user['_id']},
+        {"$set": {
+            "balance": compute_user_balance(user['_id'])}
+        }
+    )
+    print(f"{updated.modified_count} balance updated.")
     return True
