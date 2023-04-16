b22b550259edaf845b0ef4ac0eb721b1782379f5
Nico <gabo.janicolo@gmail.com>
2023-04-16 15:31:25 +0800

    update balance before and after transaction

--- src/dbHelper/add_transaction.py
+++ src/dbHelper/add_transaction.py
@@ -10,10 +10,14 @@ def add_transaction(transaction):
         {"_id": transaction['source_id']},
         {"_id": transaction['destination_id']}
     ]
+    # update balance
     for user in users:
         update_balance(user)
     new_transaction = Database().collection['transactions'].insert_one(
         transaction
     )
+    # update balance again
+    for user in users:
+        update_balance(user)
     print(f"Transaction {new_transaction.inserted_id} added.")
     return new_transaction.inserted_id
