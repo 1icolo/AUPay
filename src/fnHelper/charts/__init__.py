a0c669416541d3ad5013cdfb8f8405a01ab279ec
Nico <gabo.janicolo@gmail.com>
2023-04-22 23:26:23 +0800

    Update __init__.py

--- src/fnHelper/charts/__init__.py
+++ src/fnHelper/charts/__init__.py
@@ -4,3 +4,4 @@ from .balance_line_chart import balance_line_chart
 from .transaction_amounts_chart import transaction_amounts_chart
 from .transaction_breakdown_chart import transaction_breakdown_chart
 from .total_spending_amount_chart import total_spending_amount_chart
+from .transaction_volume_bar_chart import transaction_volume_bar_chart
