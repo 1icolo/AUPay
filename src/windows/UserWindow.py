024fb6ddca41f9d17cc88c3e4c2838485bd1b457
Nico <gabo.janicolo@gmail.com>
2023-04-24 03:31:24 +0800

    added capability for user to change their otp and password upon registration

--- src/windows/UserWindow.py
+++ src/windows/UserWindow.py
@@ -5,7 +5,7 @@ from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from dbHelper.compute_user_balance import *
 from dbHelper.find_transaction import *
-from fnHelper import export_window_to_pdf, setDateRangeFields
+from fnHelper import cryptography, export_window_to_pdf, setDateRangeFields
 from fnHelper.charts import (balance_line_chart, 
                              item_frequency_pie_chart,
                              total_spending_amount_chart,
@@ -15,7 +15,76 @@ from fnHelper.load_tables import *
 from fnHelper.refresh_clear import *
 from fnHelper.textSearch import *
 from windows.ProjectMainWindow import ProjectMainWindow
+from windows.ui.ui_ChangePasswordDialog import Ui_Dialog as ChangePasswordUi_Dialog
+from windows.ui.ui_ChangeOTPDialog import Ui_Dialog as ChangeOTPUi_Dialog
+from fnHelper.otpAuth import *
+import os
+from fnHelper import update_user
 
+
+class ChangePasswordDialog(QDialog):
+    def __init__(self, user, parent=None):
+        super(ChangePasswordDialog, self).__init__(parent)
+        self.ui = ChangePasswordUi_Dialog()
+        self.ui.setupUi(self)
+        self.ui.button_change_password.clicked.connect(lambda: self.change_password(user))
+
+    def change_password(self, user):
+        if self.ui.line_new_password.text() != "" and self.ui.line_confirm_password.text() != "" and self.ui.line_old_password.text() != "":
+            if user['password'] == cryptography.hash(self.ui.line_old_password.text()):
+                if self.ui.line_new_password.text() == self.ui.line_confirm_password.text():
+                    userData = {
+                        '_id': ObjectId(user['_id']),
+                        'card_id': user['card_id'],
+                        'school_id': user['school_id'],
+                        'password': cryptography.hash(self.ui.line_confirm_password.text()),
+                        'otp_key': user['otp_key'],
+                        'user_type': user['user_type'],
+                    }
+                    update_user(userData)
+                    QMessageBox.information(self, "Success", f"Password Changed")
+                    self.close()
+                else:
+                    QMessageBox.warning(self, "Warning", "New password must be the same with the confirmation")
+            else:
+                QMessageBox.warning(self, "Warning", "Old password is incorrect")
+        else:
+            QMessageBox.warning(self, "Warning", "All fields are required")
+
+class ChangeOTPDialog(QDialog):
+    def __init__(self, user, parent=None):
+        super(ChangeOTPDialog, self).__init__(parent)
+        self.ui = ChangeOTPUi_Dialog()
+        self.ui.setupUi(self)
+        self.generate(user)
+        self.ui.button_confirm_otp.clicked.connect(lambda: self.change_otp(user))
+
+    def generate(self, user):
+        self.secret_key = get_random_secret()
+        self.totp = get_totp(self.secret_key)
+        # self.ui.qrcodeimg.setPixmap(QPixmap(generate_qr(self.secret_key, user['school_id'])))
+        generate_qr(self.secret_key, user['school_id'])
+        self.ui.qrcodeimg.setPixmap(QPixmap('temp/qr_code.png'))
+        os.remove('temp/qr_code.png')
+
+    def change_otp(self, user):
+        verification_otp = verify_otp(self.totp, self.ui.line_otp.text())
+        verification_password = user['password'] == cryptography.hash(self.ui.line_password.text())
+        print(verification_password)
+        if verification_otp and verification_password:
+            userData = {
+                        '_id': ObjectId(user['_id']),
+                        'card_id': user['card_id'],
+                        'school_id': user['school_id'],
+                        'password': user['password'],
+                        'otp_key': self.secret_key,
+                        'user_type': user['user_type'],
+                    }
+            update_user(userData)
+            QMessageBox.information(self, "Success", "OTP updated")
+            # self.close()
+        else:
+            QMessageBox.warning(self, "Warning", "Password or OTP is incorrect")
 
 def refresh_navbar(self: ProjectMainWindow, user):
     self.userWindow_schoolIdLine.setText(user['school_id'])
@@ -23,6 +92,7 @@ def refresh_navbar(self: ProjectMainWindow, user):
     self.navHome_user.clicked.connect(lambda: self.stackedWidget_user.setCurrentIndex(0))
     self.navAnalytics_user.clicked.connect(lambda: self.stackedWidget_user.setCurrentIndex(1))
     self.navTransactions_user.clicked.connect(lambda: self.stackedWidget_user.setCurrentIndex(2))
+    self.navAccount_user.clicked.connect(lambda: self.stackedWidget_user.setCurrentIndex(3))
 
 
 def refresh_analytics(self: ProjectMainWindow, user):
@@ -53,10 +123,17 @@ def refresh_all(self: ProjectMainWindow, user):
     refresh_transactions(self, user)
     refresh_analytics(self, user)
 
+def check_otp_and_password(self: ProjectMainWindow, user):
+    if not user['password'] == cryptography.hash('Shine On, Dear AUP!'):
+        self.password_status_client.setText("Already Set")
+    if not user['otp_key'] == "":
+        self.secret_status_client.setText("Already Set")
+
 
 def UserWindow(self: ProjectMainWindow, user):
     print(__name__)
     refresh_all(self, user)
+    check_otp_and_password(self, user)
     self.navRefresh_user.clicked.connect(lambda: refresh_all(self, user))
     self.userWindow_transaction_search.textChanged.connect(lambda text: searchChanged(self, user, text))
     self.dateFrom_user.dateChanged.connect(lambda: dateChanged(self, user))
@@ -64,3 +141,5 @@ def UserWindow(self: ProjectMainWindow, user):
     self.exportPDF_user.clicked.connect(lambda: export_window_to_pdf(self, user))
     self.exportCSV_user.clicked.connect(lambda: export_to_csv(self.userWindow_transactions_table, user))
     self.buttonClearTransactions_user.clicked.connect(lambda: refresh_transactions(self, user))
+    self.change_password_client.clicked.connect(lambda: ChangePasswordDialog(user).exec())
+    self.change_otp_client.clicked.connect(lambda: ChangeOTPDialog(user).exec())
