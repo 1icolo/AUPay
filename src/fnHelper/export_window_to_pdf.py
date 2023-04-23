fca5f9baec90c1ccf9d6dd45c994958208bde700
Nico <gabo.janicolo@gmail.com>
2023-04-23 19:54:23 +0800

    created a temp folder for temporary creation of files

--- src/fnHelper/export_window_to_pdf.py
+++ src/fnHelper/export_window_to_pdf.py
@@ -8,11 +8,11 @@ def export_window_to_pdf(window, user):
     screenshot = screen.grabWindow(window.winId())
     file_name = f"{user['school_id']}_{datetime.now().strftime('%m-%d-%Y_%H-%M-%S')}"
 
-    screenshot.save("screenshot.png", "png")
+    screenshot.save("temp/screenshot.png", "png")
 
     save_file_name, _ = QtWidgets.QFileDialog.getSaveFileName(None, 'Save PDF', f'{file_name}.pdf', 'PDF Files (*.pdf)')
     if save_file_name:
         with open(save_file_name, "wb") as f:
-            f.write(img2pdf.convert("screenshot.png"))
+            f.write(img2pdf.convert("temp/screenshot.png"))
 
-    os.remove("screenshot.png")
+    os.remove("temp/screenshot.png")
