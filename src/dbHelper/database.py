e01aa17a8dc76fba9e4f193bc0a7cc69c50113d2
Nico <gabo.janicolo@gmail.com>
2023-04-23 19:54:43 +0800

    changed the pattern of otp_key to base32 from base16

--- src/dbHelper/database.py
+++ src/dbHelper/database.py
@@ -70,6 +70,7 @@ user_schema = {
             },
             'otp_key': {
                 'bsonType': 'string',
+                'pattern': '^[A-Z2-7]+=*$'
             },
             'user_type': {
                 'enum': ['admin', 'user', 'business', 'teller']
