e58d8af6887b060d0dc49c650f79486a28d0fc9e
Nico <gabo.janicolo@gmail.com>
2023-04-26 01:02:36 +0800

    fixed a bug incase user has no balance

--- src/fnHelper/checkBalanceSufficiency.py
+++ src/fnHelper/checkBalanceSufficiency.py
@@ -1,6 +1,9 @@
 from dbHelper.compute_user_balance import compute_user_balance
 
 def checkBalanceSufficiency(user_id, transactionAmount):
-    if compute_user_balance(user_id) < transactionAmount:
-        return False
-    return True
+    try:
+        if compute_user_balance(user_id) < transactionAmount:
+            return False
+        return True
+    except Exception as e:
+        print(f"check balance sufficiency error.\n{e}")
