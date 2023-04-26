47491e2a8393f88f7bd15703bb8dd933ec6d2069
Nico <gabo.janicolo@gmail.com>
2023-04-27 02:36:44 +0800

    Update setDateRangeFields.py

--- src/fnHelper/setDateRangeFields.py
+++ src/fnHelper/setDateRangeFields.py
@@ -1,7 +1,7 @@
 from PyQt5.QtCore import QDate
 from PyQt5.QtWidgets import QDateEdit
 
-def setDateRangeFields(dateFrom: QDateEdit, dateTo: QDateEdit):
+def semestral(dateFrom: QDateEdit, dateTo: QDateEdit):
     current_month = QDate.currentDate().month()
     current_year = QDate.currentDate().year()
     if 1 <= current_month <= 5:
@@ -10,6 +10,13 @@ def setDateRangeFields(dateFrom: QDateEdit, dateTo: QDateEdit):
         dateFrom.setDate(QDate(current_year, 6, 1))
     else:
         dateFrom.setDate(QDate(current_year, 8, 1))
+    dateTo.setDate(QDate.currentDate())
+    
+
+def monthly(dateFrom: QDateEdit, dateTo: QDateEdit):
+    current_month = QDate.currentDate().month()
+    current_year = QDate.currentDate().year()
+    dateFrom.setDate(QDate(current_year, current_month, 1))
     dateTo.setDate(QDate.currentDate())
 
 
